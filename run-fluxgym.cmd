10ecbb333f6539cd59dcd09ef0aeea863243536f
ridgt <rafaelideleon@gmail.com>
2024-12-31 11:41:02 -0800

    bash script for flexgym setup

---
+++ run-fluxgym.cmd
@@ -0,0 +1,2 @@
+cd fluxgym
+python app.py
--- setup-scripts/setup-fluxgym.cmd
+++ setup-scripts/setup-fluxgym.cmd
@@ -1,14 +1,11 @@
+cd ..
+
 git clone https://github.com/cocktailpeanut/fluxgym
 cd fluxgym
 git clone -b sd3 https://github.com/kohya-ss/sd-scripts
 
-# windows setup
 python -m venv env
 env\Scripts\activate
-
-# Linux
-#python -m venv env
-#source env/bin/activate
 
 cd sd-scripts
 pip install -r requirements.txt
